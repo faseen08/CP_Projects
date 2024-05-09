d575823282ddaa51edd6af6f6a855b7235b6aa66
Mohammed Faseen <muhammedfaseen94@gmail.com>
2024-05-09 12:28:41 +0530

    New files added

---
+++ Fact_Recursion.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int multiplyNumbers(int n); 
+int main()
+
+{
+
+int n;
+
+printf("Enter a positive integer: ");
+
+scanf("%d",&n);
+printf("Factorial of %d =%d", n,multiplyNumbers(n));
+
+}
+
+int multiplyNumbers(int n)
+
+{
+
+if (n>=1)
+
+return n*multiplyNumbers(n-1);
+
+else
+
+return 1;
+
+}
