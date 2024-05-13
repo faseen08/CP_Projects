7dfdedef6119ba1dc983e23d34bc02e232366937
mohammed faseen <muhammedfaseen94@gmail.com>
2024-05-14 00:20:43 +0530

    paliandrome error fixed

--- paliandrome.c
+++ paliandrome.c
@@ -3,37 +3,10 @@
 
 int main()
 {
-    char a[100],b[100],c[100],e[100];
-    char d[]="paul";
-    char f[]="Green Paradise";
+    char a[100],b[100];
+   
     int l,i,j;
 
-    printf("Enter your name : ");
-    scanf("%[^\n]%*c",c);
-
-    if(strcmp(c,d)==0)
-    {
-        printf("\nLisan Al Gaib \n");
-        printf("Mahdie what do you forsee for us?\n");
-        scanf("%[^\n]%*c",e);
-        
-        
-        
-        if(strcmp(e,f)==0)
-        {
-            for(i=0;i<100;i++)
-            {
-            printf("Lisan_al_gaib \t");
-            }
-        }
-
-        
-               
-    }
-    else
-    {
-        printf("Hello %s \n",c);
-    }
 
     printf("\nEnter a word : ");
     scanf("%s",a);
@@ -48,7 +21,7 @@ int main()
         b[i]=a[j];
         j++;
     }
-    b[i]='\0';
+    b[j]='\0';
 
     printf("a = %s \n",a);
     printf("b = %s \n",b);
